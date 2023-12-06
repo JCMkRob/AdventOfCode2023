e16a29a7689fb1ecefd9652d8988ee145342beb2
JCMkRob <joshua.cmk.robertson@gmail.com>
2023-12-06 13:06:58 -0700

    Added day 6 solution

--- Playground/Playground/Program.cs
+++ Playground/Playground/Program.cs
@@ -3,7 +3,7 @@ using AdventOfCode.Support;
 
 // YearBuilder.CreateEmptyInputs();
 
-//Wrapper.Test(typeof(Day05));
+Wrapper.Test(typeof(Day06));
 
-//Wrapper.Run(typeof(Day05), part: 1);
-Wrapper.Run(typeof(Day05), part: 2);
+Wrapper.Run(typeof(Day06), part: 1);
+Wrapper.Run(typeof(Day06), part: 2);
--- Playground/Playground/Solutions/Day06.cs
+++ Playground/Playground/Solutions/Day06.cs
@@ -1,35 +1,113 @@
-using System.CodeDom.Compiler;
 using AdventOfCode.Support;
 
 namespace AdventOfCode.Solutions;
 
 public static class Day06
 {
+    public record RaceRecord(double Time, double Distance)
+    {
+    }
+
     public static double PartOneTest(string s) => PartOne(s.Split("\n").Select(s => s.TrimEnd()));
 
-    [Example(solver: nameof(PartOneTest), solution: -1)]
+    [Example(solver: nameof(PartOneTest), solution: 288)]
     public static readonly string PartOneExample = 
             """
-
+            Time:      7  15   30
+            Distance:  9  40  200
             """;
 
     public static double PartTwoTest(string s) => PartTwo(s.Split("\n").Select(s => s.TrimEnd()));
 
-    [Example(solver: nameof(PartTwoTest), solution: -1)]
+    [Example(solver: nameof(PartTwoTest), solution: 71503)]
     public static readonly string PartTwoExample = 
             """
-
+            Time:      7  15   30
+            Distance:  9  40  200
             """;
 
     [Solution(part: 1)]
-    public static double PartOne(IEnumerable<string> strings)
+    public static double PartOne(IEnumerable<string> input)
     {
-        return 0;
+        var records = ParseInputPartOne(input);
+
+        return records.Select(WaysToBeatTheRecord).Aggregate((a , b) => a * b);
     }
 
     [Solution(part: 2)]
-    public static double PartTwo(IEnumerable<string> strings)
+    public static double PartTwo(IEnumerable<string> input)
     {
-        return 0;
+        var records = ParseInputPartTwo(input);
+
+        return records.Select(WaysToBeatTheRecord).Aggregate((a , b) => a * b);
     }
+
+    private static double WaysToBeatTheRecord(RaceRecord r)
+    {
+        double negativeRoot = (r.Time - Math.Sqrt(Math.Pow(r.Time, 2) - 4 * r.Distance)) / 2;
+        double positiveRoot = (r.Time + Math.Sqrt(Math.Pow(r.Time, 2) - 4 * r.Distance)) / 2;
+
+        double upperRoot = UpperRoot(r, positiveRoot);
+        double lowerRoot = LowerRoot(r, negativeRoot);
+
+        return upperRoot - lowerRoot + 1;
+    }
+
+    private static double LowerRoot(RaceRecord r, double button)
+    {
+        double floor = double.Floor(button);
+        double ceiling = double.Ceiling(button);
+
+        if (floor == ceiling) ceiling++;
+        
+        bool lowerRootValid = CheckRoot(r, floor);
+        bool upperRootValid = CheckRoot(r, ceiling);
+
+        if (lowerRootValid) return floor;
+        if (upperRootValid) return ceiling;
+        
+        throw new Exception();
+    }
+
+    private static double UpperRoot(RaceRecord r, double button)
+    {
+        double floor = double.Floor(button);
+        double ceiling = double.Ceiling(button);
+
+        if (floor == ceiling) floor--;
+
+        bool lowerRootValid = CheckRoot(r, floor);
+        bool upperRootValid = CheckRoot(r, ceiling);
+
+        if (upperRootValid) return ceiling;
+        if (lowerRootValid) return floor;
+
+        throw new Exception();
+    }
+
+    private static bool CheckRoot(RaceRecord r, double button) => 0 > (Math.Pow(button, 2) - (button * r.Time) + r.Distance);
+  
+    private static IEnumerable<RaceRecord> ParseInputPartOne(IEnumerable<string> input)
+    {
+        var raceRecords = input.ToArray();
+
+        var time = raceRecords[0].Split(':')[1].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(double.Parse).ToArray();
+        var dist = raceRecords[1].Split(':')[1].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(double.Parse).ToArray();
+
+        for(int index = 0; index < time.Length; index++)
+        {
+            yield return new(Time: time[index], Distance: dist[index]);
+        }
+    } 
+
+    private static IEnumerable<RaceRecord> ParseInputPartTwo(IEnumerable<string> input)
+    {
+        var raceRecords = input.ToArray();
+
+        var time = raceRecords[0].Split(':')[1].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Aggregate((a, b) => a + b);
+        var dist = raceRecords[1].Split(':')[1].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Aggregate((a, b) => a + b);
+
+        
+        yield return new(Time: double.Parse(time), Distance: double.Parse(dist));
+    } 
 }
